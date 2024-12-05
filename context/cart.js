63beadf682c8f6149fb751ff8d53439cf224b1cf
rahmiddinasiydinov <rahmiddinasiydinov@gmail.com>
2024-12-05 18:57:02 +0500

    issue resolve

--- context/cart.js
+++ context/cart.js
@@ -6,8 +6,11 @@ export function CartProvider({ children }) {
     const [cart, setCart] = useState([])
     useEffect(() => {
         if (typeof window !== 'undefined' && window.localStorage) {
+            console.log("Hello");
             const currentCart = JSON.parse(localStorage.getItem("cart"));
+            
             setCart(currentCart || []);
+            
         }
     }, [])
     useEffect(()=>{
@@ -22,8 +25,11 @@ export function CartProvider({ children }) {
     const handleSave = (newCart) => {
             if (typeof window !== "undefined" && window.localStorage) {
                 localStorage.setItem("cart", JSON.stringify(newCart))
+                "Hello"
             }
     }
+
+
     return <Context.Provider value={{ cart, updateCart }}>{children}</Context.Provider>
 }
 
